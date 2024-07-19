2c8380f8a39de30a50a4909207f9475e22e5bd2d
QpacTeam <qpac.cansat@gmail.com>
2024-07-19 23:41:25 +0200

    fixing

--- Server/Utility.py
+++ Server/Utility.py
@@ -3,9 +3,11 @@ import time
 
 def timer(function):
     def wrapper(*args, **kwargs):
-        act_time = time.time()
+        act_time: float = time.time()
         function(*args, **kwargs)
-        delta_time = time.time() - act_time
-        print(f"{function} :: {delta_time}")
+        delta_time: float = time.time() - act_time
+        print(f"{function} -> {delta_time}")
+
+    # new line of code
 
     return wrapper
