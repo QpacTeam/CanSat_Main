000f54ce4439ffe1db73906f8837f38dfa0c4237
QpacTeam <qpac.cansat@gmail.com>
2024-09-20 19:39:47 +0200

    Making Server_Run.py pretty!

--- Server/Sender/Server_Run.py
+++ Server/Sender/Server_Run.py
@@ -1,4 +1,5 @@
-from Server_Variables import STATE_FILE
+from Server_Variables import STATE_FILE, CONTROL_SLEEP
+import serial as SER
 
 
 def Get_Operation_Mode() -> int:
@@ -10,6 +11,16 @@ def Get_Operation_Mode() -> int:
         return 999
 
 
+def Radio_Write(data = '\r\n') -> None:
+    msg: str = str(data).encode('utf-8') #fordítás a lora nyelvére
+    SER.write(msg)
+    time.sleep(CONTROLL_SLEEP)
+    
+
+def Radio_Init() -> None:
+    Radio_Write('\r\n')
+    Radio_Write('sys reset\r\n')
+    Radio_Write('radio rx 0\r\n')
 
 def run():
     # TODO make the package sending code
