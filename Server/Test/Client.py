1a246d5d3d0f109f83a2c31cb73dc15bca14e7dc
Barni <barnabasszilagyi1@gmail.com>
2024-09-25 20:50:05 +0200

    vlmi

--- Server/Test/Client.py
+++ Server/Test/Client.py
@@ -1,11 +1,14 @@
 import socket as soc
 
-HOST = '192.168.0.104'
+HOST = '172.21.13.220'
 
-PORT = 9896
+PORT = 9004
 
 socket = soc.socket(soc.AF_INET, soc.SOCK_STREAM)
 socket.connect((HOST, PORT))
 
-socket.send("Hello World!".encode('utf-8'))
-print(socket.recv(1024))
+while 1:
+    alma = socket.recv(1024).decode("utf-8")
+    print(alma)
+    socket.send(f"{alma}".encode('utf-8'))
+
