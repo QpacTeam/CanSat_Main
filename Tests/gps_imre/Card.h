744ec00ad71cfee873351f929bfdb445033a47b3
Barni <barnabasszilagyi1@gmail.com>
2024-09-27 08:57:05 +0200

    q

--- Tests/gps_imre/Card.h
+++
@@ -1,7 +0,0 @@
-#ifndef SD_H_INCLUDED
-#define SD_H_INCLUDED
-
-void SD_Init(void);
-void SD_run(String File_SD, String Data_SD);
-
-#endif  // SD_H_INCLUDED
--- Tests/gps_imre/LoRa.h
+++
@@ -1,7 +0,0 @@
-#ifndef LORA_H_INCLUDED
-#define LORA_H_INCLUDED
-
-void LoRa_Init(void);
-void LoRa_Run(int Typ, const char* Data);
-
-#endif  // GPS_TIMER_H_INCLUDED
