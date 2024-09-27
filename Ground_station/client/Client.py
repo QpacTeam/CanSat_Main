c41b287a715c66c88f8c46f8b440e5f67c95b1eb
Barni <barnabasszilagyi1@gmail.com>
2024-09-27 09:05:54 +0200

    w

--- Ground_station/client/Client.py
+++ Ground_station/client/Client.py
@@ -1,4 +1,4 @@
-import socket as soc, antiparcer, icecream as ic
+import socket as soc, antiparcer, icecream as ic, graph
 
 HOST = '172.21.13.220'
 
@@ -7,9 +7,14 @@ PORT = 9090
 socket = soc.socket(soc.AF_INET, soc.SOCK_STREAM)
 socket.connect((HOST, PORT))
 
+temp_alt = graph.Function(2, [[0, 1500], [-10, 40]])
+
+datalist = []
 while 1:
-    alma = socket.recv(1024).decode("utf-8")
+    line = socket.recv(1024).decode("utf-8")
     socket.send("szam1".encode('utf-8'))
-    print(antiparcer.antiparcer_data(alma))
+    datalist = antiparcer.antiparcer_data(line)
+
+
 
 
