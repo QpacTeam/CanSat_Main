a818a072c742908ce4a980e7f186c2a1d4b6d028
lorant <qpac.cansat@gmail.com>
2024-10-24 20:29:14 +0000

    Kalman filer fuck yeeeeeeeeeaaaaaaaaaahhhhhhcd CanSat_Main/cd CanSat_Main/cd CanSat_Main/!

--- Kalman_Filter/Kalman_Filter.ino
+++ Kalman_Filter/Kalman_Filter.ino
@@ -10,6 +10,16 @@ double Ts = 0.004;
 float cal;
 float IMU_Data[size];
 
+const float C_R = 0.10;
+const float C_P = 0.14;
+const float C_W = -0.005;
+
+const float C_X = 0;
+const float C_Y = 0;
+const float C_Z = 0.01;
+
+int counter = 0; 
+
 
 void kalman(float KState, float KUncertainty, float KInput, float KMeasurement){
   KState = KState + Ts * KInput;
@@ -46,27 +56,42 @@ void setup() {
 
 void loop(void) {
   if (IMU.accelerationAvailable()) { IMU.readAcceleration(Ax, Ay, Az);
+
+    Ax -= C_X;
+    Ay -= C_Y;
+    Az -= C_Z;
+
     IMU_Data[0] = Ax;
     IMU_Data[1] = Ay;
     IMU_Data[2] = Az;
 
-    ARoll = atan(Ay/sqrt(Ax*Ax + Az*Az)) / 3.14 * 180;
-    APitch = atan(-1*Ax/sqrt(Ay*Ay + Az*Az));
-    //AYaw = atan(Az/sqrt(Ax*Ax + Ay*Ay));
-    //Serial.println(AnglePitch/3.14*180);
-    //Serial.println(AngleRoll/3.14*180);
-    //Serial.println(AngleYaw/3.14*180);
+    //Serial.println(Ax);
+    //Serial.println(Ay);
+    //Serial.println(Az);
+
+    ARoll = atan(Ay/sqrt(Ax*Ax + Az*Az)) / PI * 180;
+    APitch = atan(-1*Ax/sqrt(Ay*Ay + Az*Az)) / PI * 180;
+
+    //Serial.println(Ax);
+    //Serial.println(Ay);
+    //Serial.println(Az);
 
   }
   if (IMU.gyroscopeAvailable()) { IMU.readGyroscope(RRoll, RPitch, RYaw);
+    
+    RRoll  -= C_R;
+    RPitch -= C_P;
+    RYaw   -= C_W;
+
     IMU_Data[3] = RRoll;
     IMU_Data[4] = RPitch;
     IMU_Data[5] = RYaw;
 
-    //Serial.println(RRoll - cal);
+    //Serial.println("");
+    //Serial.println(RRoll);
     //Serial.println(RPitch);
     //Serial.println(RYaw);
-  
+
   }
 
   kalman(KARoll, KUARoll, RRoll-cal, ARoll);
@@ -76,9 +101,21 @@ void loop(void) {
   //KAPitch = Output[0];
   //KUAPitch = Output[1];
 
+  //delay(1);
   delay(Ts*1000);
-  Serial.print("Kalman: ");
-  Serial.println(KARoll);
-  Serial.print("Acc: ");
-  Serial.println(ARoll);
+  Serial.print("Variable_1:");
+  Serial.print(KARoll);
+  Serial.print(",");
+  Serial.print("Variable_2:");
+  Serial.print(ARoll);
+  Serial.print(",");
+  Serial.print("Variable_3:");
+  Serial.print(-90);
+  Serial.print(",");
+  Serial.print("Variable_4:");
+  Serial.println(90);
+
+ 
+
+
 }
